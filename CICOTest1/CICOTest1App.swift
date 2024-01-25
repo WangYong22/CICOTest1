cb326046fcc72ae1ee4f650ed5e9eb36cbda48f9
WangYong22 <102638006+WangYong22@users.noreply.github.com>
2024-01-25 12:16:35 +0800

    Initial Commit

---
+++ CICOTest1/CICOTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  CICOTest1App.swift
+//  CICOTest1
+//
+//  Created by wy on 2024/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CICOTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
