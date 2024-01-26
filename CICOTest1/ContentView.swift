13f5076a70bf031e1d40c14efa2aa10acd86b989
WangYong22 <102638006+WangYong22@users.noreply.github.com>
2024-01-26 10:17:23 +0800

    change 5

--- CICOTest1/ContentView.swift
+++ CICOTest1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, CICO4world!")
+            Text("Hello, CICO5world!")
         }
         .padding()
     }
